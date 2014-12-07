c64f5a9c562cfc6715786d5b8eb9c21aec3e8a6d
SeffyVon <fyj0823>
2014-12-07 20:13:53 +0000

    <DEV> Config of IP addr

--- src/config/Config.java
+++ src/config/Config.java
@@ -1,7 +1,7 @@
 package config;
 
 public class Config {
-	public static String macAddrString = "10.115.217.178";//"172.20.10.2";//"172.20.10.2";//"10.8.40.49"; //"10.115.193.30";
+	public static String macAddrString = "172.20.10.2";//"10.115.217.178";//"172.20.10.2";//"10.8.40.49"; //"10.115.193.30";
 	public static String linuxAddrString = "172.20.10.3";//"10.115.209.74";
 	
 	public static int macPort = 15126;
